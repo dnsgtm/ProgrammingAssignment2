d660db50a2a4a7bc8868b752b565c176b8451512
Dinesh Gautam <gautamdinesh27@gmail.com>
2016-04-30 22:08:49 +0545

    function definition added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1.set the value of the vector
+## 2.get the value of the vector
+## 3.set the value of the mean
+## 4.get the value of the mean
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixinv <- NULL
+  set <- function(y) {
+    x <<- y
+    matrixinv <<- NULL
+  }
+  get <- function() x
+  setmatrixinverse <- function(matrixinverse) matrixinv <<- matrixinverse
+  getmatrixinverse <- function() matrixinv
+  list(set = set, get = get,setmatrixinverse = setmatrixinverse,getmatrixinverse = getmatrixinverse)
 }
 
 
-## Write a short comment describing this function
+
+## This function returns Inverse of a square Matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the matrixi nverse of 'x'
+  matrixinv <- x$getmatrixinverse()
+  if (!is.null(matrixinv)) {
+    message("getting the data from cache")
+    return(matrixinv)
+  }
+  mat <- x$get()
+  matrixinv <- solve(mat, ...)
+  x$setmatrixinverse(matrixinv)
+  matrixinv
 }
